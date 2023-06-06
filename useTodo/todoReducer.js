174d631e7ad6b6bc983f9645485cb1966862fac3
Alejandro Thon <alejandro96121@hotmail.com>
2023-06-05 22:22:45 -0400

    capteta useTodo agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,27 @@
+
+export const todoReducer = (initialState = [], action) => {
+
+    switch (action.type) {
+        case '[TODO] add todo':
+            return [...initialState, action.payload]
+
+        case '[TODO] remove todo':
+            return initialState.filter(todo => todo.id !== action.payload);
+
+        case '[TODO] Toogle todo':
+            return initialState.map((todo) => {
+                if (todo.id == action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo
+            });
+
+        default:
+            return initialState;
+    }
+
+
+}
